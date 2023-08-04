1f5a43debc05abecc0e0fa187a76c3b8cd237d46
Rajat Gupta <dguptarajat@gmail.com>
2023-08-04 14:19:36 +0530

    add new smoothie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState([]);
+
+  useEffect(() => {
+    fetchSmoothies();
+  }, []);
+
+  const fetchSmoothies = async () => {
+    const { data, error } = await supabase.from("smoothies").select("*");
+    console.log(data, error);
+    if (error) {
+      setFetchError("Could not fetch data");
+      setSmoothies([]);
+      console.log(error);
+    }
+    if (data) {
+      setSmoothies(data);
+      setFetchError(null);
+    }
+  };
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies.length > 0 && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies?.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
